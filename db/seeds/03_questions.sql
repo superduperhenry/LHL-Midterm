ea8ac555b341f94ab15f0f54f29c2f7c20189a7a
Henry Truong <henry.trb@gmail.com>
2023-05-05 21:10:37 -0400

    add db files, more quizzes

--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -3,6 +3,21 @@ VALUES (1, 'What is the capital of Australia?', 'Canberra'),
 (1, 'Which African country is known as the "Land of a Thousand Hills"?', 'Rwanda'),
 (1, 'Which is the longest river in South America?','Amazon' ),
 (1, 'What is the highest mountain peak in North America?', 'Mount Denali'),
-(1, 'Which country has the largest population in the world?', 'India');
+(1, 'Which country has the largest population in the world?', 'India'),
+(2, 'What year was the very first model of the iPhone release?', '2007'),
+(2, 'Which video game console released in 2006 pioneered hte use of motion controls in its gameplay?', 'Nintendo Wii'),
+(2, 'What does "HTTP" stand for?','HyperText Transfer Protocol' ),
+(2, 'Which email service is owned by Microsoft?', 'Hotmail'),
+(2, 'What is the shortcut for the "copy" function on most computers?', 'ctrl+c'),
+(3, 'What is the symbol for potassium?', 'K'),
+(3, 'Which planet is the hottest in the solar system?', 'Venus'),
+(3, 'Which natural disaster is measured with a Richter scale?', 'Earthquake'),
+(3, 'Which animals are pearls found in?', 'Oysters'),
+(3, 'Who discovered penicillin?', 'Alexander Fleming'),
+(4, 'What other name does "corn" go by?', 'Maize'),
+(4, 'What is the primary ingredient in hummus?', 'Chickpeas'),
+(4, 'What kind of alcohol is Russia known for?', 'Vodka'),
+(4, 'Which country produces the most coffee in the world?', 'Brazil'),
+(4, 'Which European nation was said to invent hot dogs?', 'Germany');
 
 
